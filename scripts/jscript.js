af1d2fda383fad3bddc0071f4ef89b4a06b718d4
Jon M <32684774+BatNipples@users.noreply.github.com>
2018-01-14 11:45:55 +0000

    Final Website
    re arranged folders, small changes to JS, added local storage

--- scripts/jscript.js
+++ scripts/jscript.js
@@ -3,29 +3,52 @@ window.onload = function() {
 }
 
 	var slideIndex = 1;
+	var Dark = 'off';
 	//var galleryIndex = 1;
 
 function init(){
 	console.log('init fired');
 	showSlides(slideIndex);
+	darkMode(Dark);
 	
+	
+	//changes background colour depending on the value of Dark
+	function darkMode(n) {
+		if (Dark == 'on') {document.body.style.backgroundColor = "black";}
+		if (Dark =='off') {document.body.style.backgroundColor = "white";}
+	}
+	
+	//sets a local variable to store the users dark mode preference
+	darkModeToggle.addEventListener("change",function(e) {
+		console.log('initialized');
+		if ((document.getElementById("darkModeToggle").checked) == true) {localStorage.setItem('darkMode',"on"); Dark = localStorage.getItem('darkMode')}
+		if ((document.getElementById("darkModeToggle").checked) == false) {localStorage.setItem('darkMode',"off"); Dark = localStorage.getItem('darkMode')}
+		document.body.style.backgroundColor = "black";
+		darkMode(Dark);
+		
+	});
+	//links canvas to Index
 	$('#customizerLink').click(function(e) {
 		window.location.href = "canvas.html";
 	});
 	
+	
+	//displays the text and title for the currently selected gallery
 	$('.galleryImage').click(function(e) {
 		var id = $(this).data('gallery');
 		document.getElementById("galleryTextTitle").innerHTML = 'This is displaying the Title of gallery number ' + id;
 		document.getElementById("galleryBody").innerHTML = 'description of gallery number ' + id;
 	});
 	
+	
+	//focuses the windows on the selected element
 	$('.dropDownItems').click(function(e) {
 		var id = $(this).data('item');
-		console.log('ready');
 		
 		if (id == 1) {
 			document.getElementById("slideShowContainer1").focus();
 			console.log(id);}
+		//focusing on the element correctly but not panning the window to the right position because of the positioning of the elements
 		if (id == 2) {
 			document.getElementById("galleryContainer").focus();
 			console.log(id);}
@@ -37,12 +60,14 @@ function init(){
 			console.log(id);}
 	});
 	
+	
+	//changes the slide to the selected one
 	$('.dot').click(function(e){
 		var id = $(this).data('id');
 		showSlides(slideIndex = id);
 	});
 	
-	
+	//function to switch images in a carousel fashion in a slide show
 	function showSlides(n) {
 		//
 		/*stepper variable*/
